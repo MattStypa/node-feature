4fbdc6d22a84d960f5c219aa4322599fa9f61b77
Matt Stypa <mstypa@dose.com>
2016-04-07 10:20:31 -0500

    Restructured the module to handle contexts more appropriately.

--- src/feature.js
+++ src/feature.js
@@ -1,16 +1,198 @@
 module.exports = feature;
 
-var _core = require('./core');
+var _ = require('lodash');
+var roller = require('../src/roller');
 
 /**
- * Returns configured core object
+ * Get a configured features object that exposes getDigest function
  *
- * @param {object} config
+ * @param {object} features
  * @returns {object}
  */
-function feature(config) {
+function feature(features) {
+    return {
+        features: _getParsedFeatureConfig(features),
+        getDigest: function(context, overrides) {
+            return _getDigest(this.features, context, overrides);
+        }
+    };
+}
 
-    _core.setFeatures(config);
+/**
+ * Gets features digest
+ *
+ * @param {object} features
+ * @param {string} context
+ * @param {object|string} overrides
+ * @returns {Object}
+ * @private
+ */
+function _getDigest(features, context, overrides) {
+    overrides = overrides || {};
 
-    return _core;
+    return _.mapValues(_.assign(features, _getParsedOverrides(overrides)), function(value, key) {
+        return _getVariant(value, context + key);
+    });
+}
+
+/**
+ * Gets variant of a feature in a context
+ *
+ * @param {object} feature
+ * @param {string} context
+ * @returns {string|null}
+ * @private
+ */
+function _getVariant(feature, context) {
+    var variants = Object.keys(feature);
+
+    if (!variants.length) {
+        return null;
+    }
+
+    // First variant is a sure winner
+    if (feature[variants[0]] >= 100) {
+        return variants[0];
+    }
+
+    // No chance of winning
+    if (feature[variants[variants.length - 1]] === 0) {
+        return null;
+    }
+
+    var roll = roller.roll(context);
+
+    for (var variant in feature) {
+        if (roll < feature[variant]) {
+            return variant.toString();
+        }
+    }
+
+    return null;
+}
+
+/**
+ * Gets parsed features configuration
+ *
+ * @param {object} features
+ * @returns {object}
+ * @private
+ */
+function _getParsedFeatureConfig(features) {
+    if (typeof features !== 'object') {
+        throw Error('Invalid configuration');
+    }
+
+    var parsedFeatureConfig = {};
+
+    for (var feature in features) {
+        parsedFeatureConfig[feature] = _getParsedSingleFeatureConfig(features[feature]);
+    }
+
+    return parsedFeatureConfig;
+}
+
+/**
+ * Gets parsed configuration of a single feature
+ *
+ * @param {object} feature
+ * @returns {object}
+ * @private
+ */
+function _getParsedSingleFeatureConfig(feature) {
+    // Shorthand
+    if (typeof feature !== 'object') {
+        return {'on': _getSanitizedOdds(feature)};
+    }
+
+    var parsedFeatureConfig = {};
+    var totalOdds = 0;
+    var variant;
+
+    // Auto distribute
+    if (Array.isArray(feature)) {
+        var autoOdds = 100 / feature.length;
+
+        for (variant in feature) {
+            totalOdds += autoOdds;
+            parsedFeatureConfig[feature[variant]] = totalOdds;
+        }
+
+        // Ensure complete coverage
+        parsedFeatureConfig[feature[variant]] = 100;
+
+        return parsedFeatureConfig;
+    }
+
+    // Full
+    for (variant in feature) {
+        totalOdds += _getSanitizedOdds(feature[variant]);
+        parsedFeatureConfig[variant] = totalOdds;
+    }
+
+    return parsedFeatureConfig;
+}
+
+/**
+ * Gets parsed feature configuration from overrides
+ *
+ * @param {object|string} overrides
+ * @returns {Object}
+ * @private
+ */
+function _getParsedOverrides(overrides) {
+    var parsedOverrides = {};
+
+    try {
+        parsedOverrides = JSON.parse(overrides);
+    } catch (e) {
+        parsedOverrides = overrides;
+    }
+
+    if (typeof parsedOverrides !== 'object') {
+        parsedOverrides = _getParsedStringOverrides(overrides);
+    } else {
+        parsedOverrides = _.mapValues(parsedOverrides, function(value) {
+            var newValue = {};
+            newValue[value] = 100;
+
+            return newValue;
+        });
+    }
+
+    return _getParsedFeatureConfig(parsedOverrides);
+}
+
+/**
+ * Gets parsed feature configuration from string based overrides
+ *
+ * @param {string} overrides
+ * @returns {object}
+ * @private
+ */
+function _getParsedStringOverrides(overrides) {
+    return overrides.toString().split(',').reduce(function(obj, value) {
+        value = value.split(':');
+        if (value.length > 1) {
+            obj[value[0]] = {};
+            obj[value[0]][value[1]] = 100;
+        } else {
+            obj[value[0]] = 100;
+        }
+        return obj;
+    }, {});
+}
+
+/**
+ * Gets a normalized odds
+ *
+ * @param {*} odds
+ * @returns {number}
+ * @private
+ */
+function _getSanitizedOdds(odds) {
+    if (isNaN(odds)) {
+        return 0;
+    }
+    return odds < 0 ? 0 : odds;
 }
--- tests/feature-test.js
+++ tests/feature-test.js
@@ -2,21 +2,107 @@ jest.dontMock('../src/feature');
 
 describe('feature', function() {
 
-    var _feature = require('../src/feature');
-    var _core = require('../src/core');
+    var feature = require('../src/feature');
+    var roller = require('../src/roller');
 
-    it('should export constructor', function() {
-        expect(typeof _feature).toEqual('function');
+    var _featureConfig = {
+        featureA: 0,
+        featureB: 100,
+        featureC: {
+            variantA: 0,
+            variantB: 20,
+            variantC: 20,
+            variantD: 60
+        },
+        featureD: {
+            variantA: 10,
+            variantB: 10
+        },
+        featureE: {},
+        featureF: {
+            variantA: -100
+        },
+        featureG: {
+            variantA: 'test'
+        },
+        'featureH': [
+            'variantA',
+            'variantB',
+            'variantC',
+        ]
+    };
+
+    var _overrides = {
+        featureA: 'variantA',
+        featureC: 'variantE',
+        featureH: 'variantA',
+    };
+
+    beforeEach(function() {
+        roller.roll.mockClear();
     });
 
-    it('should set feature configuration', function() {
-        var configuration = {'feature': 100};
+    it('should export instantiated feature object', function() {
+        var features = feature({});
 
-        _feature(configuration);
-        expect(_core.setFeatures.mock.calls[0][0]).toEqual(configuration);
+        expect(typeof features).toEqual('object');
+        expect(typeof features.getDigest).toEqual('function');
     });
 
-    it('should return core', function() {
-        expect(_feature({})).toEqual(_core);
+    it('should throw error on invalid configuration', function() {
+        expect(function() {
+            feature('invalid');
+        }).toThrow();
+    });
+
+    it('should get variant digest ', function() {
+        roller.roll.mockReturnValue(30);
+        var features = feature(_featureConfig);
+
+        expect(features.getDigest('context')).toEqual({
+            'featureA': null,
+            'featureB': 'on',
+            'featureC': 'variantC',
+            'featureD': null,
+            'featureE': null,
+            'featureF': null,
+            'featureG': null,
+            'featureH': 'variantA'
+        });
+    });
+
+    it('should pass context to roller', function() {
+        var features = feature(_featureConfig);
+        features.getDigest('context');
+
+        expect(roller.roll.mock.calls[0][0]).toEqual('contextfeatureC');
+    });
+
+    it('should apply overrides from json', function() {
+        var features = feature(_featureConfig);
+        var digest = features.getDigest('context', _overrides);
+
+        expect(digest.featureA).toEqual('variantA');
+        expect(digest.featureC).toEqual('variantE');
+        expect(digest.featureH).toEqual('variantA');
+    });
+
+    it('should apply overrides from json string', function() {
+        var features = feature(_featureConfig);
+        var digest = features.getDigest('context', JSON.stringify(_overrides));
+
+        expect(digest.featureA).toEqual('variantA');
+        expect(digest.featureC).toEqual('variantE');
+        expect(digest.featureH).toEqual('variantA');
+    });
+
+    it('should apply overrides from string', function() {
+        var features = feature(_featureConfig);
+        var digest = features.getDigest('context', 'featureA,featureB:variantA,featureC:variantE,featureH:variantA');
+
+        expect(digest.featureA).toEqual('on');
+        expect(digest.featureB).toEqual('variantA');
+        expect(digest.featureC).toEqual('variantE');
+        expect(digest.featureH).toEqual('variantA');
     });
 });
